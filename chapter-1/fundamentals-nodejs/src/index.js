2992e58922e5772f1ea152a2ddf984e4fc184187
EDusik <eduardodusik@gmail.com>
2021-06-01 14:32:40 -0300

    01-02 (05) - types of params

--- chapter-1/fundamentals-nodejs/src/index.js
+++ chapter-1/fundamentals-nodejs/src/index.js
@@ -2,12 +2,21 @@ const express = require('express');
 
 const app = express();
 
+// to be able to use JSON in the application
+app.use(express.json());
+
 /**
+# Requisition Methods
 * GET - Get an information
 * POST - Insert information
 * PUT - Change an information
-* DELETE - Delete an information
 * PATCH - Change specific information
+* DELETE - Delete an information
+
+# Types of params
+  Route Params => (Identify, edit, search or delete an recurse)
+  Query Params => (Filter / Pagination)
+  Body params =>  (Objects to Insert / Change - JSON)
 */
 
 // app.get('/', (request, response) => {
@@ -18,14 +27,20 @@ const app = express();
 // });
 
 app.get("/courses", (request, response) => {
+  const query = request.query;
+  console.log(query);
 	return response.json(["Course 1", "Course 2", "Course 3"]);
 });
 
 app.post("/courses", (request, response) => {
+  const body = request.body;
+  console.log(body);
 	return response.json(["Course 1", "Course 2", "Course 3", "Course 4"]);
 });
 
 app.put("/courses/:id", (request, response) => {
+  const params = request.params;
+  console.log(params);
 	return response.json(["Course 5", "Course 2", "Course 3", "Course 4"]);
 });
 
