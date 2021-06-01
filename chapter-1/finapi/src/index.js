3c4e4b3645c9850f797fe9a50a64372a894b8073
EDusik <eduardodusik@gmail.com>
2021-06-01 15:05:55 -0300

    01-02-01 (02) - account registration

--- chapter-1/finapi/src/index.js
+++ chapter-1/finapi/src/index.js
@@ -1,5 +1,23 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
+const customers = [];
+
+app.use(express.json());
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+  const id = uuidv4();
+
+  customers.push({
+    id,
+    cpf,
+    name,
+    statement: []
+  });
+
+  return response.status(201).send();
+});
 
 app.listen(3333);
