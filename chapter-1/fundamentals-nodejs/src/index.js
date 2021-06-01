cffa260b02a5b06b462ec02bd14e08b38203edf3
EDusik <eduardodusik@gmail.com>
2021-06-01 11:25:54 -0300

    01-02 (03) - using http methods

--- chapter-1/fundamentals-nodejs/src/index.js
+++ chapter-1/fundamentals-nodejs/src/index.js
@@ -2,11 +2,63 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  // return response.send('Hello world!');
-  return response.json({
-    message: 'Hello World Ignite!'
-  });
+/**
+* GET - Get an information
+* POST - Insert information
+* PUT - Change an information
+* DELETE - Delete an information
+* PATCH - Change specific information
+*/
+
+// app.get('/', (request, response) => {
+//   // return response.send('Hello world!');
+//   return response.json({
+//     message: 'Hello World Ignite - Fundamentals NodeJs!'
+//   });
+// });
+
+app.get('/courses', (request, response) => {
+  return response.json([
+    'Course 1',
+    'Course 2',
+    'Course 3'
+  ]);
 });
+
+app.post('/courses', (request, response) => {
+  return response.json([
+    'Course 1',
+    'Course 2',
+    'Course 3',
+    'Course 4'
+  ]);
+});
+
+app.put('/courses/:id', (request, response) => {
+  return response.json([
+    'Course 5',
+    'Course 2',
+    'Course 3',
+    'Course 4'
+  ]);
+});
+
+app.patch('/courses/:id', (request, response) => {
+  return response.json([
+    'Course 1',
+    'Course 2',
+    'Course 3',
+    'Course 4.4'
+  ]);
+});
+
+app.delete('/courses/:id', (request, response) => {
+  return response.json([
+    'Course 1',
+    'Course 2',
+    'Course 3'
+  ]);
+});
+
 
 app.listen(3333);
