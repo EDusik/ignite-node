ce59e317782b5f5e918bee198ca46a1e6aa12acd
EDusik <eduardodusik@gmail.com>
2021-06-02 16:28:25 -0300

    01-02-01 (07) - adding middlewares

--- chapter-1/finapi/src/index.js
+++ chapter-1/finapi/src/index.js
@@ -4,9 +4,29 @@ const { v4: uuidv4 } = require('uuid');
 const app = express();
 const customers = [];
 
+// Middleware
+function verifyIfExistsAccountCpf(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (!customer) {
+    return response.status(400).json({ 
+      error: "Customer not found"
+    })
+  }
+
+  request.customer = customer;
+
+  return next();
+}
+
 app.use(express.json());
 
 app.post("/account", (request, response) => {
+
   const { cpf, name } = request.body;
 
   const customerAlreadyExists = customers.some(
@@ -29,18 +49,13 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
-app.get("/statement", (request, response) => {
-  const { cpf } = request.headers;
-
-  const customer = customers.find(customer => customer.cpf === cpf);
-
-  if (!customer) {
-    return response.status(400).json({ 
-      error: "Customer not found"
-    })
-  }
+app.get("/statement", verifyIfExistsAccountCpf, (request, response) => {
+  const { customer } = request;
 
   return response.json(customer.statement);
 });
 
+// Always use middleware
+// app.use(verifyIfExistsAccountCpf);
 app.listen(3333);
+
