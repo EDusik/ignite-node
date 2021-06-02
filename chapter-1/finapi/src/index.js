b0a39cf92d116f3bd81dd5b89e0c43f0908f970a
EDusik <eduardodusik@gmail.com>
2021-06-02 14:25:05 -0300

    01-02-01 (04) - validating existing CPF

--- chapter-1/finapi/src/index.js
+++ chapter-1/finapi/src/index.js
@@ -8,10 +8,19 @@ app.use(express.json());
 
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
-  const id = uuidv4();
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({
+      error: "Customer already exists!"
+    });
+  }
 
   customers.push({
-    id,
+    id: uuidv4(),
     cpf,
     name,
     statement: []
