44d10602a4c8bee886cdcb6527b44c2736890edf
EDusik <eduardodusik@gmail.com>
2021-06-02 18:51:37 -0300

    01-02-01 (10) - listing statement by date

--- chapter-1/finapi/src/index.js
+++ chapter-1/finapi/src/index.js
@@ -105,7 +105,19 @@ app.post("/withdraw", verifyIfExistsAccountCpf, (request, response) => {
   return response.status(201).send();
 });
 
+app.get("/statement/date", verifyIfExistsAccountCpf, (request, response) => {
+  const { customer } = request;
+  const { date } = request.query;
+  
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter((statement) => 
+    statement.created_at.toDateString() === new Date(dateFormat).toDateString()
+  );
+
+  return response.json(statement);
+});
+
 // Always use middleware
 // app.use(verifyIfExistsAccountCpf);
 app.listen(3333);
-
